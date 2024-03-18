7aa753e0f555783fd56ac9bd7dc00fef3593d33f
chandu <cs8074970147@gmail.com>
2024-03-18 14:12:14 +0000

    ASSIGNMENT 5

---
+++ src/components/ProfileDetails/index.js
@@ -0,0 +1,67 @@
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import './index.css'
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
+const ProfileDetails = props => {
+  const renderProfile = () => {
+    const {profileDetails} = props
+    const {profileImageUrl} = profileDetails
+    return (
+      <div className="profile-details-container">
+        <img
+          src="https://res.cloudinary.com/ddvqjo4f2/image/upload/v1710666333/WhatsApp_Image_2024-03-17_at_14.33.00_24e2631e_rarf2p.jpg"
+          alt="profile"
+          className="profile-image"
+        />
+        <h1 className="profile-name">Chandrasekhar Kuruva</h1>
+        <p className="profile-bio">
+          Lead Software Developer and Full-stack expert
+        </p>
+      </div>
+    )
+  }
+
+  const renderProfileFailure = () => {
+    const {getProfileDetails} = props
+    return (
+      <div className="profile-failure-container">
+        <button
+          className="retry-button"
+          type="button"
+          onClick={getProfileDetails}
+        >
+          Retry
+        </button>
+      </div>
+    )
+  }
+
+  const renderProfileLoader = () => (
+    <div className="loader-container-profile" data-testid="loader">
+      <Loader type="ThreeDots" color="#ffffff" height="50" width="50" />
+    </div>
+  )
+
+  const {profileApiStatus} = props
+
+  switch (profileApiStatus) {
+    case apiStatusConstants.inProgress:
+      return renderProfileLoader()
+    case apiStatusConstants.success:
+      return renderProfile()
+    case apiStatusConstants.failure:
+      return renderProfileFailure()
+    default:
+      return null
+  }
+}
+
+export default ProfileDetails
